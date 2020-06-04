9f2a69459003a70aa54bf9aa194bc2b83916c571
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-06-04 16:19:27 +0530

    SfMaskedEdit sample committed.

---
+++ Samples/Appearance/ViewModel/ViewModel.cs
@@ -0,0 +1,78 @@
+﻿
+using Syncfusion.Windows.Shared;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Appearance
+{
+    class ViewModel : NotificationObject
+    {
+        private FlowDirection flowDirection;
+        private Brush background = Brushes.Yellow;
+        private Brush selectionBrush = Brushes.Orange;
+        private Brush foreground = Brushes.Blue;
+        private Brush caretBrush = Brushes.Red;
+        private Brush borderBrush = Brushes.Pink;
+
+        public Brush Background
+        {
+            get { return background; }
+            set
+            {
+                background = value;
+                this.RaisePropertyChanged(nameof(Background));
+            }
+        }
+        public Brush SelectionBrush
+        {
+            get { return selectionBrush; }
+            set
+            {
+                selectionBrush = value;
+                this.RaisePropertyChanged(nameof(SelectionBrush));
+            }
+        }
+        public Brush Foreground
+        {
+            get { return foreground; }
+            set
+            {
+                foreground = value;
+                this.RaisePropertyChanged(nameof(Foreground));
+            }
+        }
+        public Brush BorderBrush
+        {
+            get { return borderBrush; }
+            set
+            {
+                borderBrush = value;
+                this.RaisePropertyChanged(nameof(BorderBrush));
+            }
+        }
+        public Brush CaretBrush
+        {
+            get { return caretBrush; }
+            set
+            {
+                caretBrush = value;
+                this.RaisePropertyChanged(nameof(CaretBrush));
+            }
+        }
+
+        public FlowDirection FlowDirection
+        {
+            get { return flowDirection; }
+            set
+            {
+                flowDirection = value;
+                this.RaisePropertyChanged(nameof(FlowDirection));
+            }
+        }
+
+        public ViewModel()
+        {
+
+        }
+    }
+}
---
+++ Samples/InputOptions/ViewModel/ViewModel.cs
@@ -0,0 +1,95 @@
+﻿using Syncfusion.Windows.Controls.Input;
+using Syncfusion.Windows.Shared;
+using System.Windows.Media;
+using MaskFormat = Syncfusion.Windows.Controls.Input.MaskFormat;
+
+namespace InputOptions
+{
+    class ViewModel : NotificationObject
+    {
+        private bool showPromptOnFocus = true;
+        private MaskFormat valueMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+        private object _value;
+        private InputValidationMode validationMode = InputValidationMode.KeyPress;
+        private Brush errorBorderBrush = Brushes.Yellow;
+        private string mask= @"\+1 \([0-9]\d{2}\) [0-9]\d{2}-[0-9]\d{3}";
+
+        public bool ShowPromptOnFocus
+        {
+            get
+            {
+                return showPromptOnFocus;
+            }
+            set
+            {
+                showPromptOnFocus = value;
+                this.RaisePropertyChanged(nameof(ShowPromptOnFocus));
+            }
+        }
+
+        public object Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                this.RaisePropertyChanged(nameof(Value));
+            }
+        }
+
+        public MaskFormat ValueMaskFormat
+        {
+            get
+            {
+                return valueMaskFormat;
+            }
+            set
+            {
+                valueMaskFormat = value;
+                this.RaisePropertyChanged(nameof(ValueMaskFormat));
+            }
+        }
+
+        public InputValidationMode ValidationMode
+        {
+            get
+            {
+                return validationMode;
+            }
+            set
+            {
+                validationMode = value;
+                this.RaisePropertyChanged(nameof(ValidationMode));
+            }
+        }
+
+        public Brush ErrorBorderBrush
+        {
+            get
+            {
+                return errorBorderBrush;
+            }
+            set
+            {
+                errorBorderBrush = value;
+                this.RaisePropertyChanged(nameof(ErrorBorderBrush));
+            }
+        }
+
+        public string Mask
+        {
+            get
+            {
+                return mask;
+            }
+            set
+            {
+                mask = value;
+                this.RaisePropertyChanged(nameof(Mask));
+            }
+        }
+    }
+}
